8b6081e348ec1895cfc2f6843043a72100ed5805
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-02-24 18:51:43 -0500

    lab completed

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  counter = 0
+  modified_array = []
+  while counter < array.length
+    modified_array.push(yield array[counter])
+    counter += 1
+  end
 
+  return modified_array
 
+end
